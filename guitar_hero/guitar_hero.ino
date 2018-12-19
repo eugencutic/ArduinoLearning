07cca610fe356f5e8e7218a5a609bc299118eb79
Eugen Cutic <eugen_cutic@yahoo.com>
2018-12-19 15:53:02 +0200

    added display and wait for input to start. still nothing working properly (:

--- guitar_hero/guitar_hero.ino
+++ guitar_hero/guitar_hero.ino
@@ -1,12 +1,16 @@
+
 #include "LedControl.h" //  need the library
 #include <SimpleList.h>
 #include "pitches.h"
+#include <LiquidCrystal.h>
 
 #define BUTTON_LEFT_PIN A3
 #define BUTTON_MIDDLE_PIN A4
 #define BUTTON_RIGHT_PIN A5
 #define PIEZO_PIN 8
+#define V0_PIN 9 // PWN in loc de POTENTIOMETRU
 
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 LedControl lc = LedControl(12, 11, 10, 1); //DIN, CLK, LOAD, No. DRIVER
  
 // pin 12 is connected to the MAX7219 pin 1
@@ -122,6 +126,18 @@ struct NotesManager
   bool wasPressed[3] = {false, false, false};
 
   int score = 0;
+  int lives = 5;
+
+  void reset()
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      while (!noteColumns[i].empty())
+        noteColumns[i].pop_back();
+    }
+    score = 0;
+    lives = 5;
+  }
 
   void addNewNote(int col)
   {
@@ -207,37 +223,34 @@ struct NotesManager
   
   void updateColumnScore(int col)
   {
-    //song.playNextNote();
+    
     
     Note lastNoteOnColumn = *(noteColumns[col].end() - 1);
-    Serial.println("***");
     if (lastNoteOnColumn.left.currY == 5)
     {
       score += 5;
-      Serial.println(lastNoteOnColumn.left.currY);
-      //lastNoteOnColumn.hide();
+      song.playNextNote();
       hideLastThree(col);
       noteColumns[col].pop_back();
     }
     else if (lastNoteOnColumn.left.currY == 6)
     {
       score += 10;
-      Serial.println(lastNoteOnColumn.left.currY);
-      //lastNoteOnColumn.hide();
+      song.playNextNote();
       hideLastThree(col);
       noteColumns[col].pop_back();
     }
     else if (lastNoteOnColumn.left.currY == 7)
     {
       score += 5;
-      Serial.println(lastNoteOnColumn.left.currY);
-      //lastNoteOnColumn.hide();
+      song.playNextNote();
       hideLastThree(col);
       noteColumns[col].pop_back();
     }
     else
     {
       score -= 5;
+      lives --;
     }
   }
 
@@ -267,33 +280,100 @@ void setup()
   pinMode(BUTTON_LEFT_PIN, INPUT);
   pinMode(BUTTON_MIDDLE_PIN, INPUT);
   pinMode(BUTTON_RIGHT_PIN, INPUT);
-  noteManager.toggleNoteBar(true);
+  
+
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(1, 1);
+ 
+  pinMode(V0_PIN, OUTPUT); 
+  analogWrite(V0_PIN, 100);
+
+  lcd.print("hello, world!");
   
 }
 
+
+bool playing = false;
 void loop()
 {
-  song.playNextNote();
   
-  //delay(100);
   Serial.print("SCORE: ");
-  Serial.println(noteManager.score);
-  int col = random(0, 3);
-  noteManager.addNewNote(col);
+  Serial.print(noteManager.score);
+  Serial.print(" LIVES: ");
+  Serial.println(noteManager.lives);
 
+  if (playing)
+  {
+    int col = random(0, 3);
+    noteManager.addNewNote(col);
+
+    nextTurn();
+
+    if (noteManager.lives <= 0)
+      playing = false;
+    else
+    {
+      nextTurn();
+
+      if (noteManager.lives <= 0)
+      playing = false;
+    }
+  }
+  else
+  {
+    waitForStart();
+  }
+  
+}
+
+void nextTurn()
+{
   delay(200);
   noteManager.readInputAndUpdateScore(0);
   noteManager.readInputAndUpdateScore(1);
   noteManager.readInputAndUpdateScore(2);
   
   noteManager.moveAllDown();
-  
-  delay(200);
+}
 
-  noteManager.readInputAndUpdateScore(0);
-  noteManager.readInputAndUpdateScore(1);
-  noteManager.readInputAndUpdateScore(2);
+void waitForStart()
+{
+  int reading;
+
+  reading = digitalRead(BUTTON_MIDDLE_PIN);
+  if(reading == HIGH)
+  {
+    playing = true;
+    noteManager.toggleNoteBar(true);
+    noteManager.reset();
+    return;
+  }
+  waitAnimation();
+}
+
+void waitAnimation()
+{
+  lc.clearDisplay(0);
+  for(int i = 2; i <= 5; i++)
+  {
+    lc.setLed(0, 2, i, true);
+    lc.setLed(0, 5, i, true);
+  }
+  for(int i = 2; i <= 5; i++)
+  {
+    lc.setLed(0, i, 2, true);
+    lc.setLed(0, i, 5, true);
+  }
+  delay(500);
   
-  noteManager.moveAllDown();
+  lc.clearDisplay(0);
+  
+  lc.setLed(0, 3, 3, true);
+  lc.setLed(0, 3, 4, true);
+  lc.setLed(0, 4, 3, true);
+  lc.setLed(0, 4, 4, true);
+  delay(500);
+  lc.clearDisplay(0);
   
 }
