d2400a7dee1fcdb5189333e9e69b27b99937df2c
Eugen Cutic <eugen_cutic@yahoo.com>
2018-12-20 16:59:27 +0200

    changed delay to millis, added 'you lose' animation with sound, code for display, audio feedback for wrong note

--- guitArduino/guitArduino.ino
+++ guitArduino/guitArduino.ino
@@ -1,5 +1,5 @@
 
-#include "LedControl.h" //  need the library
+#include "LedControl.h" 
 #include <SimpleList.h>
 #include "pitches.h"
 #include <LiquidCrystal.h>
@@ -18,30 +18,19 @@ LedControl lc = LedControl(12, 11, 10, 1); //DIN, CLK, LOAD, No. DRIVER
 // pin 10 is connected to LOAD pin 12
 // 1 as we are only using 1 MAX7219
 
+//Controls the visual representation of one Point
 struct Point
 {
   int currX = 0;
   int currY = 0;
-  int prevX = 0;
-  int prevY = 0;
-  bool movedX = false;
-  bool movedY = false;
   
   void moveX(int x)
   {
-    prevX = currX;
     currX = x;
-    movedX = true;
   }
   void moveY(int y)
   {
-    prevY = currY;
     currY = y;
-    movedY = true;
-  }
-  void setNotMoved()
-  {
-    movedX = movedY = false;
   }
   void show()
   {
@@ -54,6 +43,7 @@ struct Point
   
 };
 
+//Controls the visual representation of one Note
 struct Note
 {
    Point left;
@@ -96,6 +86,7 @@ struct Note
    }
 };
 
+//Struct to iterate through the melody's notes
 struct Song
 {
   int melody[16] = {
@@ -115,6 +106,9 @@ struct Song
   
 } song;
 
+
+//Struct which controls notes movement, their sync with user input,
+//as well as sound, using the Song struct
 struct NotesManager
 {
   SimpleList<Note> noteColumns[3];
@@ -122,11 +116,10 @@ struct NotesManager
   int buttonStates[3];
   int lastButtonStates[3] = {LOW, LOW, LOW};
   int lastDebounceTimes[3] = {0, 0, 0};
-  int debounceDelay = 25;
-  bool wasPressed[3] = {false, false, false};
+  int debounceDelay = 50;
 
   int score = 0;
-  int lives = 5;
+  int lives = 15;
 
   void reset()
   {
@@ -136,7 +129,7 @@ struct NotesManager
         noteColumns[i].pop_back();
     }
     score = 0;
-    lives = 5;
+    lives = 15;
   }
 
   void addNewNote(int col)
@@ -165,6 +158,8 @@ struct NotesManager
       lc.setLed(0, 6, 5, on_off);
   }
 
+  //Reads button input and correlates it with note positions to update score
+  //col: 0-2 value representing note columns, NOT actual matrix columns
   void readInputAndUpdateScore(int col)
   {
     int reading;
@@ -199,17 +194,13 @@ struct NotesManager
       {
         buttonStates[col] = reading;
 
-        if (buttonStates[col] == HIGH)// && !wasPressed[col])
+        if (buttonStates[col] == HIGH)
         {
-          //Serial.println(col);
           toUpdate = true;
-          wasPressed[col] = true;
         }
         if(buttonStates[col] == LOW)
         {
           toUpdate = false;
-          wasPressed[col] = false;
-          //Serial.println(-col);
         }
           
       }
@@ -220,104 +211,158 @@ struct NotesManager
 
     lastButtonStates[col] = reading;
   }
-  
+
+  //Correlates the press of one button with the corresponding notes column
+  //and updates score accordingly:
+  //+10 on bar; +5 above and underneath it; -5 anywhere else 
   void updateColumnScore(int col)
   {
-    
     
     Note lastNoteOnColumn = *(noteColumns[col].end() - 1);
     if (lastNoteOnColumn.left.currY == 5)
     {
       score += 5;
       song.playNextNote();
-      hideLastThree(col);
+      lastNoteOnColumn.hide();
       noteColumns[col].pop_back();
     }
     else if (lastNoteOnColumn.left.currY == 6)
     {
       score += 10;
       song.playNextNote();
-      hideLastThree(col);
+      lastNoteOnColumn.hide();
       noteColumns[col].pop_back();
     }
     else if (lastNoteOnColumn.left.currY == 7)
     {
       score += 5;
       song.playNextNote();
-      hideLastThree(col);
+      lastNoteOnColumn.hide();
       noteColumns[col].pop_back();
     }
     else
     {
+      tone(PIEZO_PIN, NOTE_F2, 100);
       score -= 5;
       lives --;
     }
   }
-
-  void hideLastThree(int col)
-  {
-    lc.setLed(0, 5, col * 3, false);
-    lc.setLed(0, 5, col * 3 + 1, false);
-    
-    lc.setLed(0, 6, col * 3, false);
-    lc.setLed(0, 6, col * 3 + 1, false);
-    
-    lc.setLed(0, 7, col * 3, false);
-    lc.setLed(0, 7, col * 3 + 1, false);
-  }
 }noteManager;
 
+void setupLcdInfo()
+{
+  lcd.setCursor(11, 0);
+  lcd.print("SCORE");
 
+  lcd.setCursor(11, 1);
+  lcd.print("LIVES");
+
+  lcd.setCursor(0, 0);
+  lcd.print("0");
+  lcd.setCursor(0, 1);
+  lcd.print("15");
+}
 
 void setup()
 {
   Serial.begin(9600);
-  // the zero refers to the MAX7219 number, it is zero for 1 chip
+  
   lc.shutdown(0, false); // turn off power saving, enables display
   lc.setIntensity(0, 2); // sets brightness (0~15 possible values)
   lc.clearDisplay(0);// clear screen
+  
   randomSeed(analogRead(0));
+  
   pinMode(BUTTON_LEFT_PIN, INPUT);
   pinMode(BUTTON_MIDDLE_PIN, INPUT);
   pinMode(BUTTON_RIGHT_PIN, INPUT);
   
-
   lcd.begin(16, 2);
   lcd.clear();
-  lcd.setCursor(1, 1);
  
   pinMode(V0_PIN, OUTPUT); 
   analogWrite(V0_PIN, 100);
-
-  lcd.print("hello, world!");
   
 }
 
 
 bool playing = false;
+
+unsigned long timeTurn1 = 0;
+unsigned long timeTurn2 = 0;
+unsigned long timeAnimation1 = 0;
+unsigned long timeAnimation2 = 0;
+
+int turn = 1;
+int turnPeriod = 200;
+int animationPeriod = 500;
+int difficultyPeriod = 10000;
+unsigned long difficultyTime = 0;
+
 void loop()
 {
+
+  showScore();
+  showLives();
   
   Serial.print("SCORE: ");
   Serial.print(noteManager.score);
   Serial.print(" LIVES: ");
-  Serial.println(noteManager.lives);
-
+  Serial.print(noteManager.lives);
+  
   if (playing)
   {
-    int col = random(0, 3);
-    noteManager.addNewNote(col);
+    if (millis() > timeTurn1 + turnPeriod && turn == 1)
+    {
+      timeTurn1 = millis();
 
-    nextTurn();
-
+      int col = random(0, 3);
+      noteManager.addNewNote(col);
+      turn = 2;
+      nextTurn();
+    }
+    while (millis() < timeTurn2 + 2 * turnPeriod)
+    {
+      noteManager.readInputAndUpdateScore(0);
+      noteManager.readInputAndUpdateScore(1);
+      noteManager.readInputAndUpdateScore(2);
+    }
     if (noteManager.lives <= 0)
+    {
+      youLose();
       playing = false;
+      setupLcdInfo();
+    }
     else
     {
-      nextTurn();
+      if (millis() > timeTurn2 + 2 * turnPeriod && turn == 2)
+      {
+        timeTurn2 = millis();
+        turn = 1;
+        nextTurn();
+      }
 
       if (noteManager.lives <= 0)
-      playing = false;
+      {
+        youLose();
+        playing = false;
+        setupLcdInfo();
+      }
+    }
+
+    while (millis() < timeTurn2 + turnPeriod)
+    {
+      noteManager.readInputAndUpdateScore(0);
+      noteManager.readInputAndUpdateScore(1);
+      noteManager.readInputAndUpdateScore(2);
+    }
+
+    if(millis() > difficultyTime + difficultyPeriod)
+    {
+      difficultyTime = millis();
+      turnPeriod -= 10;
+      if (turnPeriod < 100)
+        turnPeriod = 100;
     }
   }
   else
@@ -327,53 +372,107 @@ void loop()
   
 }
 
+void showScore()
+{
+  lcd.setCursor(0, 0);
+  lcd.print(noteManager.score);
+}
+
+void showLives()
+{
+  lcd.setCursor(0, 1);
+  lcd.print(noteManager.lives);
+}
+
+void youLose()
+{
+  lc.clearDisplay(0);
+  for(int i = 0; i < 8; i++)
+  {
+    lc.setLed(0, i, i, true);
+    lc.setLed(0, i, 7 - i, true);
+  }
+  tone(PIEZO_PIN, NOTE_E4, 300);
+  delay(333);
+  tone(PIEZO_PIN, NOTE_DS4, 300);
+  delay(333);
+  tone(PIEZO_PIN, NOTE_D4, 300);
+  delay(333);
+  lc.clearDisplay(0);
+}
+
 void nextTurn()
 {
-  delay(200);
+    
   noteManager.readInputAndUpdateScore(0);
   noteManager.readInputAndUpdateScore(1);
   noteManager.readInputAndUpdateScore(2);
-  
   noteManager.moveAllDown();
+  
 }
 
-void waitForStart()
+void getStartInput()
 {
   int reading;
 
   reading = digitalRead(BUTTON_MIDDLE_PIN);
   if(reading == HIGH)
   {
+    lc.clearDisplay(0);
     playing = true;
+    timeTurn1 = millis();
+    timeTurn2 = millis();
+    turnPeriod = 200;
     noteManager.toggleNoteBar(true);
     noteManager.reset();
     return;
   }
-  waitAnimation();
 }
 
-void waitAnimation()
+void waitForStart()
 {
-  lc.clearDisplay(0);
-  for(int i = 2; i <= 5; i++)
+  if (millis() > timeAnimation1 + animationPeriod)
   {
-    lc.setLed(0, 2, i, true);
-    lc.setLed(0, 5, i, true);
+    timeAnimation1 = millis();
+    
+    lc.clearDisplay(0);
+    
+    for(int i = 2; i <= 5; i++)
+    {
+      lc.setLed(0, 2, i, true);
+      lc.setLed(0, 5, i, true);
+    }
+    for(int i = 2; i <= 5; i++)
+    {
+      lc.setLed(0, i, 2, true);
+      lc.setLed(0, i, 5, true);
+    }
   }
-  for(int i = 2; i <= 5; i++)
+
+  while (millis() < timeAnimation1 + animationPeriod)
   {
-    lc.setLed(0, i, 2, true);
-    lc.setLed(0, i, 5, true);
+    if(!playing)
+      getStartInput();
+    else
+      return;
   }
-  delay(500);
+  
+  
+  timeAnimation2 = millis();
   
   lc.clearDisplay(0);
-  
+
   lc.setLed(0, 3, 3, true);
   lc.setLed(0, 3, 4, true);
   lc.setLed(0, 4, 3, true);
   lc.setLed(0, 4, 4, true);
-  delay(500);
-  lc.clearDisplay(0);
   
+  
+  while (millis() < timeAnimation2 + animationPeriod)
+  {
+    if(!playing)
+      getStartInput();
+    else
+      return;
+  }
 }
